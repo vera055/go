fc314ad8adbbe026d05054aa50e4a53fff6b6a08
Vera <1102472738@qq.com>
2018-12-05 12:11:16 +0800

    use vuex

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -19,10 +19,12 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'HomeHeader',
-  props: {
-    city: String
+  // 把公用数据city映射到名字为city的计算属性中
+  computed: {
+    ...mapState(['city'])
   }
 }
 </script>
@@ -52,7 +54,7 @@ export default {
       border-radius: .1rem
       color: #ccc
     .header-right
-      width: 1.24rem
+      min-width: 1.24rem
       float: right
       text-align: center
       color: #fff
