d388f620f5fba3bb0c5d95f6f31362922baa43f6
Vera <1102472738@qq.com>
2019-01-16 17:59:10 +0800

    header animation

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,17 +2,19 @@
   <div>
     <div class="title">猜你喜欢</div>
     <ul>
-      <li
-          class="item border-bottom"
-          v-for="item of list"
-          :key="item.id">
+      <router-link
+        class="item border-bottom"
+        v-for="item of list"
+        :key="item.id"
+        :to="'/detail/' + item.id"
+      >
         <img class="item-img" :src="item.imgUrl">
         <div class="item-info">
           <p class="item-title">{{item.title}}</p>
           <p class="item-desc">{{item.desc}}</p>
           <button class="item-button">查看详情</button>
         </div>
-      </li>
+      </router-link>
     </ul>
   </div>
 
